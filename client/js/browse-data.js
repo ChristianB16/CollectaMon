d0bade55bef6f90f710ac387a26c3ec263c44a5e
ChristianB16 <124315535+ChristianB16@users.noreply.github.com>
2023-11-26 15:53:27 -0500

    5.2

---
+++ client/js/browse-data.js
@@ -0,0 +1,40 @@
+var cards = [];
+var activeCard = 0;
+
+const pokeUrl = "http://localhost:5000";
+
+
+var app = angular.module("browseCards", []);
+
+app.controller('browseCardsCtrl', function($scope, $http) {
+    $scope.obj = {};
+
+    $scope.get_records = function() {
+        $http({
+            method: "get",
+            url: pokeUrl + "/get-records"  
+        }).then(function(response) {
+            if (response.data.msg === "SUCCESS") {
+                cards = response.data.pokeData;
+                $scope.obj = cards[activeCard];
+                $scope.showHide();
+            } else {
+                console.log(response.data.msg);
+            }
+        }, function(response) {
+            console.log(response);
+        });
+    };
+    $scope.get_records();
+
+    $scope.changeCard = function(direction) {
+        activeCard += direction;
+        $scope.obj = cards[activeCard];
+        $scope.showHide();
+    };
+
+    $scope.showHide = function() {
+        $scope.hidePrev = (activeCard === 0) ? true : false;
+        $scope.hideNext = (activeCard === cards.length - 1) ? true : false;
+    };
+});
--- client/js/enter-data.js
+++ client/js/enter-data.js
@@ -1,48 +1,50 @@
 var pokeUrl = "http://localhost:5000";
 
-$('#submit').click(function(){
-    var name = $('#name').val();
-    var type  = $('#type').val();
-    var ability  = $('#ability').val();
-    var attack = $('#attack').val();
-    var set = $('#set').val();
-    var setNumber = $('#setNumber').val();
-    var price = $('#price').val();
+jQuery(document).ready(function($) {
+    $('#submit').click(function() {
+        var name = $('#name').val();
+        var type = $('#type').val();
+        var ability = $('#ability').val();
+        var attack = $('#attack').val();
+        var set = $('#set').val();
+        var setNumber = $('#setNumber').val();
+        var price = $('#price').val();
 
-    var jsonObject = {
-        name: name,
-        type: type,
-        ability: ability,
-        attack: attack,
-        set: set,
-        setNumber: setNumber,
-        price: price
-    }
+        var jsonObject = {
+            name: name,
+            type: type,
+            ability: ability,
+            attack: attack,
+            set: set,
+            setNumber: setNumber,
+            price: price
+        };
 
-    $.ajax({
-        url: pokeUrl + '/write-record', 
-        type: 'post',
-        data: jsonObject,
-        success: function(response){
-            var data = JSON.parse(response)
-            if(data.msg == "SUCCESS"){
-                alert("Data Saved");
-            } else {
-                console.log(data.msg);
+        $.ajax({
+            url: pokeUrl + '/write-record',
+            type: 'post',
+            data: jsonObject,
+            success: function(response) {
+                var data = JSON.parse(response);
+                if (data.msg == "SUCCESS") {
+                    alert("Data Saved");
+                } else {
+                    console.log(data.msg);
+                }
+            },
+            error: function(err) {
+                console.log(err);
             }
-        },
-        error: function(err){
-            console.log(err);
-        }
+        });
     });
-});
 
-$("#clear").click(function(){
-    $("#name").val("");
-    $("#type").val("");
-    $("#ability").val("");
-    $("#attack").val("");
-    $("#set").val("");
-    $("#setNumber").val("");
-    $("#price").val("");
+    $("#clear").click(function() {
+        $("#name").val("");
+        $("#type").val("");
+        $("#ability").val("");
+        $("#attack").val("");
+        $("#set").val("");
+        $("#setNumber").val("");
+        $("#price").val("");
+    });
 });
